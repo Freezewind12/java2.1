3a6cafeafa40c9f848525f9c84ac78c89782a97f
Igors Gončarovs <s22goncigor@vea.lv>
2024-04-05 11:45:45 +0300

    Main class changes

--- src/service/mainService.java
+++ src/service/mainService.java
@@ -1,5 +1,22 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import model.Bus;
+import model.Fuel;
+import model.Tractor;
+import model.Vehicle;
+
+
 public class mainService {
-	
+	public static ArrayList<Vehicle> allVehicles = new ArrayList<Vehicle>();
+	public static void main(String[] arhs) {
+	Bus bus1 = new Bus("Mercedes", 10000, 2, Fuel.diesel, 60, true);
+	Tractor tractor1 = new Tractor("RAM", 54321, 2, Fuel.petrol, "None", false);
+	allVehicles.addAll(Arrays.asList(bus1, tractor1));
+	for(Vehicle tempV : allVehicles) {
+		System.out.println(tempV.getClass().getName() + " -> " + tempV);
+		}
+	}	
 }
